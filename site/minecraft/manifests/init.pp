fae1f8f1b5443860b723c556e5fc36ef226cf4e8
Audible-Headache <56700761+Audible-Headache@users.noreply.github.com>
2019-10-21 15:24:04 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -20,7 +20,8 @@ class minecraft (
     file {'/etc/systemd/system/minecraft.service':
         ensure => file,
         content => epp('minecraft/minecraft.service',{
-            install_dir => $install_dir,
+            install_dir => $install_dir
+        }
     }
     service {'minecraft':
         ensure  => running,
